06bdd19962c231976473960b57ca8f587fe92da4
Bharat Medasani <mbkumar@gmail.com>
2020-11-19 17:46:59 -0500

    dofs.py and optimizable.py are merged

--- src/simsopt/core/__init__.py
+++ src/simsopt/core/__init__.py
@@ -3,10 +3,10 @@
 # Distributed under the terms of the LGPL License
 
 from .util import *
-from .optimizable import *
+from .optimizable import Optimizable, Target, optimizable
 from .surface import *
 from .functions import *
-from .dofs import *
+#from .dofs import DOFs
 from .least_squares_problem import LeastSquaresTerm, LeastSquaresProblem
 from .serial_solve import least_squares_serial_solve
 from .mpi import MpiPartition
